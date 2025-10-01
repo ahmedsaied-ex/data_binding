10620bccee56b3178ff12ab2549713266ec5499c
ahmed.saied <ahmed.saied@expertapps.com.sa>
2025-10-01 17:21:08 +0300

    refactor : from toasts to SnackBar

--- app/src/main/java/com/example/testdatabinding/ui/fragments/MapsActivity.kt
+++ app/src/main/java/com/example/testdatabinding/ui/fragments/MapsActivity.kt
@@ -7,18 +7,16 @@ import android.os.Bundle
 import android.preference.PreferenceManager
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.addCallback
+import com.google.android.material.snackbar.Snackbar
+
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.example.testdatabinding.App
 import com.example.testdatabinding.MyOpject
 import com.example.testdatabinding.data.model.TripModel
-import com.example.testdatabinding.data.view_model.TripViewModel
-import com.example.testdatabinding.data.view_model.TripViewModelFactory
 import com.example.test_trip_logic.ui.MyBottomSheet
+import com.example.testdatabinding.constants.Constants
 import com.example.testdatabinding.databinding.ActivityMapsBinding
 import org.osmdroid.config.Configuration
 import org.osmdroid.events.MapEventsReceiver
@@ -34,20 +32,14 @@ class MapsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMapsBinding
     private lateinit var osmMap: MapView
     private var locationOverlay: MyLocationNewOverlay? = null
-
-    private val tripViewModel: TripViewModel by viewModels {
-        TripViewModelFactory(application as App)
-    }
     private var currentMarker: Marker? = null
-
     private var trip: TripModel? = null
-
     private val requestPermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
             if (isGranted) {
                 enableMyLocation()
             } else {
-                Toast.makeText(this, "Location permission denied", Toast.LENGTH_LONG).show()
+                Snackbar.make(binding.root, "Location permission denied", Snackbar.LENGTH_LONG).show()
             }
         }
 
@@ -59,27 +51,28 @@ class MapsActivity : AppCompatActivity() {
             applicationContext,
             PreferenceManager.getDefaultSharedPreferences(applicationContext)
         )
-
+        Configuration.getInstance().userAgentValue = packageName
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
-        trip = intent.getParcelableExtra("trip")
-
+        trip = intent.getParcelableExtra(Constants.TRIP)
 
         osmMap = binding.osmMap
+        osmMap.setTileSource(org.osmdroid.tileprovider.tilesource.TileSourceFactory.MAPNIK)
         osmMap.setMultiTouchControls(true)
         osmMap.controller.setZoom(12.0)
+        Configuration.getInstance().userAgentValue = packageName
 
 
         val tripPoint = GeoPoint(trip?.lat ?: 0.0, trip?.lng ?: 0.0)
         osmMap.controller.setCenter(tripPoint)
 
-        val tripMarker = Marker(osmMap).apply {
+         currentMarker = Marker(osmMap).apply {
             position = tripPoint
             title = trip?.name ?: "Trip"
         }
-        osmMap.overlays.add(tripMarker)
+        osmMap.overlays.add(currentMarker)
 
 
         val mapEventsReceiver = object : MapEventsReceiver {
@@ -99,15 +92,15 @@ class MapsActivity : AppCompatActivity() {
                         osmMap.overlays.add(currentMarker)
                         osmMap.controller.setCenter(geoPoint)
 
-                        Toast.makeText(
-                            this@MapsActivity,
-                            "New location: ${geoPoint.latitude}, ${geoPoint.longitude}",
+                        Snackbar.make(
+                            binding.root,
+                            "lat: ${geoPoint.latitude},\nlng: ${geoPoint.longitude}",
                             Toast.LENGTH_SHORT
                         ).show()
                         trip = trip?.copy(lat = geoPoint.latitude , lng = geoPoint.longitude)
                         Log.d("TRIP_ID_LOG",geoPoint.latitude.toString())
                     } else {
-                        Toast.makeText(this@MapsActivity, "Can't be edited", Toast.LENGTH_SHORT).show()
+                        Snackbar.make(binding.root, "Can't be edited", Snackbar.LENGTH_SHORT).show()
                     }
                 }
                 return true
@@ -126,7 +119,7 @@ class MapsActivity : AppCompatActivity() {
 
         binding.btnBack.setOnClickListener {
             val resultIntent = Intent()
-            resultIntent.putExtra("trip_result", trip)
+            resultIntent.putExtra(Constants.TRIP_RESULT, trip)
             setResult(RESULT_OK, resultIntent)
             finish()
         }
@@ -148,7 +141,7 @@ class MapsActivity : AppCompatActivity() {
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) -> {
-                Toast.makeText(this, "Location permission needed", Toast.LENGTH_LONG).show()
+                Snackbar.make(binding.root, "Location permission needed", Snackbar.LENGTH_LONG).show()
                 requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
             }
 
