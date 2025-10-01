e25e5a437a3f60f50d1748081f93a306f7cdcb54
ahmed.saied <ahmed.saied@expertapps.com.sa>
2025-10-01 17:13:39 +0300

    refactor : my trips list

---
+++ app/src/main/java/com/example/testdatabinding/data/MyTrips.kt
@@ -0,0 +1,28 @@
+package com.example.testdatabinding.data
+
+import com.example.testdatabinding.data.model.TripModel
+
+object MyTrips {
+   val myList= mutableListOf(
+    TripModel(id = "1", name = "Cairo", time = "09:00 AM", lat = 30.0444, lng = 31.2357),
+    TripModel(id = "2", name = "Hurghada", time = "11:30 AM", lat = 27.2579, lng = 33.8116),
+    TripModel(id = "3", name = "Luxor", time = "03:15 PM", lat = 25.6872, lng = 32.6396),
+    TripModel(id = "4", name = "Aswan", time = "04:45 PM", lat = 24.0889, lng = 32.8998),
+    TripModel(id = "5", name = "Alexandria", time = "08:30 AM", lat = 31.2001, lng = 29.9187),
+    TripModel(id = "6", name = "Paris", time = "10:00 AM", lat = 48.8566, lng = 2.3522),
+    TripModel(id = "7", name = "Rome", time = "02:15 PM", lat = 41.9028, lng = 12.4964),
+    TripModel(id = "8", name = "Berlin", time = "04:00 PM", lat = 52.5200, lng = 13.4050),
+    TripModel(id = "9", name = "Amsterdam", time = "06:30 PM", lat = 52.3676, lng = 4.9041),
+    TripModel(id = "10", name = "Barcelona", time = "11:45 AM", lat = 41.3851, lng = 2.1734),
+    TripModel(id = "11", name = "New York", time = "08:00 AM", lat = 40.7128, lng = -74.0060),
+    TripModel(id = "12", name = "Los Angeles", time = "12:30 PM", lat = 34.0522, lng = -118.2437),
+    TripModel(id = "13", name = "Chicago", time = "03:00 PM", lat = 41.8781, lng = -87.6298),
+    TripModel(id = "14", name = "Miami", time = "05:15 PM", lat = 25.7617, lng = -80.1918),
+    TripModel(id = "15", name = "San Francisco", time = "07:45 AM", lat = 37.7749, lng = -122.4194),
+    TripModel(id = "16", name = "Rio de Janeiro", time = "09:20 AM", lat = -22.9068, lng = -43.1729),
+    TripModel(id = "17", name = "Buenos Aires", time = "01:10 PM", lat = -34.6037, lng = -58.3816),
+    TripModel(id = "18", name = "Lima", time = "04:50 PM", lat = -12.0464, lng = -77.0428),
+    TripModel(id = "19", name = "London", time = "06:40 PM", lat = 51.5074, lng = -0.1278),
+    TripModel(id = "20", name = "Athens", time = "07:25 AM", lat = 37.9838, lng = 23.7275)
+    )
+}
