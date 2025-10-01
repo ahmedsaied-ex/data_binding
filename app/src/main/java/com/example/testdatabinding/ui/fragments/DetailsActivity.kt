be698755005563acecc0ae0de4631f7644947462
ahmed.saied <ahmed.saied@expertapps.com.sa>
2025-10-01 17:18:48 +0300

    feat : dataBinding for details

--- app/src/main/java/com/example/testdatabinding/ui/fragments/DetailsActivity.kt
+++ app/src/main/java/com/example/testdatabinding/ui/fragments/DetailsActivity.kt
@@ -2,19 +2,14 @@ package com.example.testdatabinding.ui.fragments
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.addCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContentProviderCompat.requireContext
-import com.example.testdatabinding.App
-
-
-import com.example.testdatabinding.repository.TripRepository
-import com.example.testdatabinding.data.view_model.TripViewModel
-import com.example.testdatabinding.data.view_model.TripViewModelFactory
-
+import androidx.databinding.DataBindingUtil
+import com.example.testdatabinding.R
+import com.example.testdatabinding.constants.Constants
 import com.example.testdatabinding.data.model.TripModel
+import com.example.testdatabinding.data.view_model.DetailsTripViewModel
 import com.example.testdatabinding.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
@@ -22,49 +17,61 @@ class DetailsActivity : AppCompatActivity() {
     private lateinit var binding:
             ActivityDetailsBinding
 
-    private val tripViewModel: TripViewModel by viewModels {
-        TripViewModelFactory(application as App)
-    }
+    private val tripViewModel: DetailsTripViewModel by viewModels()
 
 
- var trip : TripModel? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityDetailsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_details)
 
+        binding.viewModel= tripViewModel
+        binding.lifecycleOwner=this
 
+        val initTrip = intent.getParcelableExtra<TripModel>(Constants.TRIP)
+        if(tripViewModel.trip.value==null){
+            tripViewModel.setTrip(initTrip ?: TripModel(
+                id = "",
+                name ="",
+                time = "",
+                lat = 0.0,
+                lng = 0.0
+            ))
 
-         trip = intent.getParcelableExtra<TripModel>("trip")
-
-        binding.trip=trip
-
-        binding.fabMap.setOnClickListener {
-            openMapsActivity(trip?:TripModel("","","",0.0,0.0))
         }
-
+        binding.fabMap.setOnClickListener {
+            openMapsActivity(tripViewModel.trip.value ?: TripModel(
+                id = "",
+                name ="",
+                time = "",
+                lat = 0.0,
+                lng = 0.0
+            )
+            )
+        }
         binding.btnBack.setOnClickListener {
             val resultIntent = Intent()
-            resultIntent.putExtra("trip_result", trip)
+            resultIntent.putExtra(Constants.TRIP_RESULT, tripViewModel.trip.value)
             setResult(RESULT_OK, resultIntent)
-            finish() // close the activity
+            finish()
         }
 
 
     }
+
     private val mapsLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == AppCompatActivity.RESULT_OK) {
-                val updatedTrip = result.data?.getParcelableExtra<TripModel>("trip_result")
+            if (result.resultCode == RESULT_OK) {
+                val updatedTrip = result.data?.getParcelableExtra<TripModel>(Constants.TRIP_RESULT)
                 updatedTrip?.let {
-                    binding.trip= it
-                    trip=it
+                    tripViewModel.setTrip(it)
+
                 }
             }
         }
+
     fun openMapsActivity(trip: TripModel) {
         val intent = Intent(this, MapsActivity::class.java)
-        intent.putExtra("trip", trip)
+        intent.putExtra(Constants.TRIP, trip)
         mapsLauncher.launch(intent)
     }
 
