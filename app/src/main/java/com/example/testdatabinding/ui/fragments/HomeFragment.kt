a6f7b8390b39b86025c4f05cd9682e38527d6ea1
ahmed.saied <ahmed.saied@expertapps.com.sa>
2025-10-01 17:17:34 +0300

    feat : dataBinding for HomeFragment

--- app/src/main/java/com/example/testdatabinding/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/testdatabinding/ui/fragments/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.testdatabinding.ui.fragments
 
+import TripAdapter
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -13,12 +14,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.testdatabinding.App
+import com.example.testdatabinding.constants.Constants
 import com.example.testdatabinding.data.model.TripModel
 
 import com.example.testdatabinding.data.view_model.TripViewModel
 import com.example.testdatabinding.data.view_model.TripViewModelFactory
 
-import com.example.testdatabinding.ui.adapter.TripAdapter
 import com.example.testdatabinding.databinding.FragmentHomeBinding
 
 
@@ -34,8 +35,7 @@ class HomeFragment : Fragment() {
     private lateinit var tripAdapter: TripAdapter
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
@@ -43,30 +43,33 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        tripAdapter = TripAdapter { trip ->
+            openDetailsActivity(trip)
+        }
+
+        binding.rvTrips.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvTrips.adapter = tripAdapter
+
         tripViewModel.trips.observe(viewLifecycleOwner) { trips ->
-            tripAdapter = TripAdapter(trips) { trip ->
-                openDetailsActivity(trip)
-            }
-            binding.rvTrips.layoutManager = LinearLayoutManager(requireContext())
-            binding.rvTrips.adapter = tripAdapter
+            tripAdapter.submitList(trips)
         }
     }
 
     private val detailsLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == AppCompatActivity.RESULT_OK) {
-                val updatedTrip = result.data?.getParcelableExtra<TripModel>("trip_result")
+                val updatedTrip = result.data?.getParcelableExtra<TripModel>(Constants.TRIP_RESULT)
                 updatedTrip?.let {
-                    tripViewModel.updateTripLocation(it.id, it.lat, it.lng)
+                    tripViewModel.updateTripLocation(id = it.id, newLat = it.lat, newLng = it.lng)
                 }
             }
         }
+
     fun openDetailsActivity(trip: TripModel) {
         val intent = Intent(requireContext(), DetailsActivity::class.java)
-        intent.putExtra("trip", trip)
+        intent.putExtra(Constants.TRIP, trip)
         detailsLauncher.launch(intent)
     }
-
 
 
     override fun onDestroyView() {
